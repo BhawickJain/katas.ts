91cc5fd457b140830c83f041fa77ec3e1d0857d2
Bhawick Jain <bhawick@gmail.com>
2022-07-13 10:07:55 +0100

    add smallest-number-of-two implementation and tests

--- src/smallest-number-of-two.spec.ts
+++ src/smallest-number-of-two.spec.ts
@@ -1,5 +1,19 @@
 import smallestNumberOfTwo from "./smallest-number-of-two";
 
+/*
+smallestNumberOfTwo(3,10) // return: 3
+smallestNumberOfTwo(2,1.9) // return: 1.9
+smallestNumberOfTwo(-10,5) // return: -10
+smallestNumberOfTwo(-0,0) // return: null
+smallestNumberOfTwo(3,3) // return: null
+
+# Comment post test case
+Javascript is treating 0 and 0 as equivalent 
+numbers so the expected return is null.
+
+
+*/
+
 test("smallestNumberOfTwo returns smallest among positive numbers", () => {
   expect(smallestNumberOfTwo(3, 10)).toBe(3);
 });
