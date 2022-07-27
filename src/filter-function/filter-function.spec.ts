13b4c6b21e44033e65fedaa69f8c3f171186c818
Bhawick Jain <bhawick@gmail.com>
2022-07-27 20:41:22 +0100

    added standard recommended tests for filter

--- src/filter-function/filter-function.spec.ts
+++ src/filter-function/filter-function.spec.ts
@@ -1,11 +1,12 @@
 import filter from "./filter-function";
 
-test("Filter return positive numbers when given an array of numbers and a isPositive function", () => {
+test("1 -- Filter return positive numbers when given an array of numbers and a isPositive function", () => {
   const isPositive = (num: number): boolean => num > 0;
   expect(filter([-1, 1, 2, -2, 3, -3], isPositive)).toStrictEqual([1, 2, 3]);
+  expect(filter([10, 50, -20, -10, 100], v => v < 11)).toStrictEqual([10, -20, -10])
 });
 
-test("Filter return string of more than 5 when given an array of strings and a isGreaterThanLengthFive function", () => {
+test("2 -- Filter return string of more than 5 when given an array of strings and a isGreaterThanLengthFive function", () => {
   const isGreaterThanLengthFive = (str: string): boolean => str.length > 5;
   expect(
     filter(
@@ -13,4 +14,5 @@ test("Filter return string of more than 5 when given an array of strings and a i
       isGreaterThanLengthFive,
     ),
   ).toStrictEqual(["greeting"]);
+  expect(filter("AbcDeFgHI".split(""), ch => ch.toUpperCase() === ch)).toStrictEqual(["A", "D", "F", "H", "I"])
 });
