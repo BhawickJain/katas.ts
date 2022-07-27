1f8171eb2934282363cbd8b88cf8c345fbef59d7
Bhawick Jain <bhawick@gmail.com>
2022-07-27 07:07:53 +0100

    grammar correction

--- src/generate-all-passwords.spec.ts
+++ src/generate-all-passwords.spec.ts
@@ -3,7 +3,7 @@ import {
   generateAllPasswords,
 } from "./generate-all-passwords";
 
-test("appendCharAsCombination return appends each char to the string separately into an array", () => {
+test("appendCharAsCombination returns appends each char to the string separately into an array", () => {
   expect(appendCharAsCombination("ab".split(""), "bb")).toStrictEqual([
     "bba",
     "bbb",
@@ -14,7 +14,7 @@ test("appendCharAsCombination return appends each char to the string separately 
 test("generateAllPasswords returns every combination possible for a charset and length", () => {
   expect(generateAllPasswords("a", 1)).toStrictEqual(["a"]);
   expect(generateAllPasswords("a", 2)).toStrictEqual(["aa"]);
-  expect(generateAllPasswords("ab", 2)).toStrictEqual(["aa", "ab", "ba", "bb"]);
+  expect(generateAllPasswords("ab", 2)).toStrictEqual(["aa", "ab", "ba", "bb"]); // in strict equal order of array matters
   expect(generateAllPasswords("ab!", 3)).toStrictEqual([
     "aaa",
     "aab",
