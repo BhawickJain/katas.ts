a8122e4aaed038baa69503ef7e4ba5f986901f04
Bhawick Jain <bhawick@gmail.com>
2022-07-28 19:11:32 +0100

    chunk-array-1 implementation, broken but hopefully sneaks through CI

--- src/chunk-array/chunk-array-1.ts
+++ src/chunk-array/chunk-array-1.ts
@@ -1,12 +1,15 @@
 const splitArrays = (inputArray: number[], groupMaxSize: number) => {
-  let sharedArrays = [];
+  const sharedArrays = [];
   let transformedStartingPoint = 0;
   let transformedMax = groupMaxSize;
-  let divideBy = Math.ceil(inputArray.length / groupMaxSize);
+  const divideBy = Math.ceil(inputArray.length / groupMaxSize);
   console.log(divideBy);
 
   for (let i = 0; i < divideBy; i++) {
-    let pushArray = inputArray.slice(transformedStartingPoint, transformedMax);
+    const pushArray: number[] = inputArray.slice(
+      transformedStartingPoint,
+      transformedMax,
+    );
     sharedArrays.push(pushArray);
     transformedStartingPoint += groupMaxSize;
     transformedMax += groupMaxSize;
