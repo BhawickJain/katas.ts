5831bbe45747cb150bde7835c0a4a5c465fb4820
Bhawick Jain <bhawick@gmail.com>
2022-08-02 09:51:43 +0100

    implement high-order function that tests multiple implementations for a problem

--- src/chunk-array/chunk-array.spec.ts
+++ src/chunk-array/chunk-array.spec.ts
@@ -1,26 +1,19 @@
 import chunkArray from "./chunk-array-0";
 import splitArrays from "./chunk-array-1";
 
-test("splitArray can return an array with an expect number of arrays", () => {
-  expect(chunkArray([1, 2, 3, 4, 5, 6, 7], 4)).toHaveLength(2);
-  expect(
-    chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 4),
-  ).toHaveLength(4);
-  expect(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)).toHaveLength(3);
-  expect(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4)).toHaveLength(3);
-  expect(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 2)).toHaveLength(4);
-  expect(chunkArray([1, 2, 3, 4, 5, 6, 7, 9], 3)).toHaveLength(3);
-  expect(chunkArray([10, 20, 30, 40, 50], 4)).toHaveLength(2);
-});
+const implementations: ((arr: number[], size: number) => {})[] = [chunkArray, splitArrays]
 
-test("splitArray can return an array with an expect number of arrays", () => {
-  expect(splitArrays([1, 2, 3, 4, 5, 6, 7], 4)).toHaveLength(2);
-  expect(
-    splitArrays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 4),
-  ).toHaveLength(4);
-  expect(splitArrays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)).toHaveLength(3);
-  expect(splitArrays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4)).toHaveLength(3);
-  expect(splitArrays([1, 2, 3, 4, 5, 6, 7, 8], 2)).toHaveLength(4);
-  expect(splitArrays([1, 2, 3, 4, 5, 6, 7, 9], 3)).toHaveLength(3);
-  expect(splitArrays([10, 20, 30, 40, 50], 4)).toHaveLength(2);
-});
+implementations.forEach((imp) => {
+  test(`${imp.toString()} can return an array with an expect number of arrays`, () => {
+    expect(imp([1, 2, 3, 4, 5, 6, 7], 4)).toHaveLength(2);
+    expect(
+      imp([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 4),
+    ).toHaveLength(4);
+    expect(imp([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)).toHaveLength(3);
+    expect(imp([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4)).toHaveLength(3);
+    expect(imp([1, 2, 3, 4, 5, 6, 7, 8], 2)).toHaveLength(4);
+    expect(imp([1, 2, 3, 4, 5, 6, 7, 9], 3)).toHaveLength(3);
+    expect(imp([10, 20, 30, 40, 50], 4)).toHaveLength(2);
+  });
+
+})
