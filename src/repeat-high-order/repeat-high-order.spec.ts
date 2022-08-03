f129bb1408044d1e6f27b6373007b360c4e193c3
Bhawick Jain <bhawick@gmail.com>
2022-08-03 08:25:59 +0100

    remove console logs from repeat high order functions

--- src/repeat-high-order/repeat-high-order.spec.ts
+++ src/repeat-high-order/repeat-high-order.spec.ts
@@ -1,7 +1,8 @@
 import repeat from "./repeat-high-order";
 
 test("repeat calls a function a certain specified number of times", () => {
-  const print = jest.fn(() => console.log("Hello"));
+  const print = jest.fn(() => {}); // empty function returns void
+  // const print = jest.fn(() => console.log("Hello")); // uncomment to see effect
   repeat(5, print);
   expect(print).toBeCalledTimes(5);
 });
