7e76a6c5d5bcec7a771ef23d2b74f5f211cc1c6f
Bhawick Jain <bhawick@gmail.com>
2022-08-04 08:51:00 +0100

    clean to restart implementation

--- src/count-occurances-top-3/convertObjectToArray.spec.ts
+++
@@ -1,5 +0,0 @@
-import convertObjectToArray from "./convertObjectToArray";
-
-test("convertObjectToArray returns an array representation of an obejct", () => {
-  expect(convertObjectToArray({ l: 2 })).toStrictEqual([["l", 2]]);
-});
--- src/count-occurances/count-occurances.spec.ts
+++ src/count-occurances/count-occurances.spec.ts
@@ -1,48 +1,20 @@
-import countOccurancesZero from "./count-occurances-0";
-import countOccurancesOne from "./count-occurances-1";
-import Dictionary from "./Dictionary";
+import countOccurances from "./count-occurances";
+import OccuranceDictionary from "./OccuranceDictionary";
 
-test("countOccurancesZero returns an array of char-count pairs", () => {
-  expect(countOccurancesZero("hello")).toStrictEqual([
-    ["h", 1],
-    ["e", 1],
-    ["l", 2],
-    ["o", 1],
-  ]);
-  expect(countOccurancesZero("mississippi")).toStrictEqual([
-    ["m", 1],
-    ["i", 4],
-    ["s", 4],
-    ["p", 2],
-  ]);
-  expect(countOccurancesZero("necessary")).toStrictEqual([
-    ["n", 1],
-    ["e", 2],
-    ["c", 1],
-    ["s", 2],
-    ["a", 1],
-    ["r", 1],
-    ["y", 1],
-  ]);
-  expect(countOccurancesZero("wass'up")).toStrictEqual([
-    ["w", 1],
-    ["a", 1],
-    ["s", 2],
-    ["'", 1],
-    ["u", 1],
-    ["p", 1],
-  ]);
-});
-
-const implementations: [(str: string) => Dictionary<number>] = [
-  countOccurancesOne,
+const implementations: [(str: string[]) => OccuranceDictionary] = [
+  countOccurances,
 ];
 
 implementations.forEach((imp) => {
   test(`${imp.name} returns an array of char-count pairs`, () => {
-    expect(imp("hello")).toStrictEqual({ h: 1, e: 1, l: 2, o: 1 });
-    expect(imp("mississippi")).toStrictEqual({ m: 1, i: 4, s: 4, p: 2 });
-    expect(imp("necessary")).toStrictEqual({
+    expect(imp("hello".split(""))).toStrictEqual({ h: 1, e: 1, l: 2, o: 1 });
+    expect(imp("mississippi".split(""))).toStrictEqual({
+      m: 1,
+      i: 4,
+      s: 4,
+      p: 2,
+    });
+    expect(imp("necessary".split(""))).toStrictEqual({
       n: 1,
       e: 2,
       c: 1,
@@ -51,7 +23,7 @@ implementations.forEach((imp) => {
       r: 1,
       y: 1,
     });
-    expect(imp("wass'up")).toStrictEqual({
+    expect(imp("wass'up".split(""))).toStrictEqual({
       w: 1,
       a: 1,
       s: 2,
@@ -59,7 +31,7 @@ implementations.forEach((imp) => {
       u: 1,
       p: 1,
     });
-    expect(imp("Elli ELLi ELLi Day".split(/W/))).toStrictEqual({
+    expect(imp("ELLi ELLi ELLi Day".split(/\W/))).toStrictEqual({
       ELLi: 3,
       Day: 1,
     });
