b2e1cbfcee45be38db5fbdb5b24b0808d1265c95
Bhawick Jain <bhawick@gmail.com>
2022-08-05 10:27:18 +0100

    reduce function implemented with tests

--- src/reduce-function/reduce-function.spec.ts
+++ src/reduce-function/reduce-function.spec.ts
@@ -9,18 +9,16 @@ test("reduce returns a concatenation of an array of numbers", () => {
   const concat = (a: number | string, b: number): string => `${a}${b}`;
   expect(reduce([1, 2, 3], concat, "")).toBe("123");
   const str = reduce([1, 2, 3], concat, "");
-  console.log(str.length);
 });
 
 test("reduce can handle empty array returns a concatenation of an array of numbers", () => {
   const concat = (a: string, b: number): string => `${a}${b}`;
   expect(reduce([], concat, "")).toBe("");
   const str = reduce([1, 2, 3], concat, "");
-  console.log(str.length);
 });
 
-// test("reduce returns a flattened array of numbers", () => {
-//     const inputArrayNested : number[][] = [[1], [2, 3], [4, 5, 6]]
-//     const flatten = <T>(a: Array<T>, b: Array<T>): Array<T> => [...a, ...b]
-//     expect(reduce(inputArrayNested, flatten)).toBe('123')
-// })
+test("reduce returns a flattened array of numbers", () => {
+    const inputArrayNested : number[][] = [[1], [2, 3], [4, 5, 6]]
+    const flatten = (a: number[], b: number[]): number[] => [...a, ...b]
+    expect(reduce(inputArrayNested, flatten, [])).toStrictEqual([1, 2, 3, 4, 5, 6])
+})
--- src/reduce-function/reduce-function.ts
+++ src/reduce-function/reduce-function.ts
@@ -1,11 +1,10 @@
-// are the T | U types the right approach? have I unnecessarily widened the time signature of prev?
 const reduce = <T, U>(
   arr: T[],
   callbackFn: (prev: U, curr: T, currentIndex?: number, arr?: T[]) => U,
   initialValue: U,
 ): U => {
-  let prev: U = initialValue
-  
+  let prev: U = initialValue;
+
   for (let i = 0; i < arr.length; i++) {
     const curr: T = arr[i];
     prev = callbackFn(prev, curr, i, arr);
