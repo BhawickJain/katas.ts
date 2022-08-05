562f80110f38cbad3cfc5a04a1519ddeb15c9beb
Bhawick Jain <bhawick@gmail.com>
2022-08-05 07:27:49 +0100

    use axios instead of fetch to make data request

---
+++ src/count-occurrences/moby-dick.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+//moby dick
+axios.get("https://www.gutenberg.org/files/2701/2701-0.txt")
+    .then(res => res.data.toString())
+    .then(processText)
+    .catch(err => console.error(err))
+
+function processText(text: string){
+    const listOfWords = text.slice(0, 100000).split(/\W/);    
+    // const result = getMostCommonWords(listOfWords, 200);
+    console.log(listOfWords);
+}
+
+// function getMostCommonWords(listOfWords, numOfWinners){
+//     //TODO: you need to implement this
+// }
