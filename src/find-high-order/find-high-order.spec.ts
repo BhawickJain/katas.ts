4fd258a7e29ab8faa1632e98f0e226c9af40df2a
Bhawick Jain <bhawick@gmail.com>
2022-08-24 10:25:41 +0100

    yarn and lint and add more tests

--- src/find-high-order/find-high-order.spec.ts
+++ src/find-high-order/find-high-order.spec.ts
@@ -3,11 +3,23 @@ import { find } from "./find-high-order";
 test("find returns the first item that matches predicate function in an array of numbers", () => {
   expect(find([1, 2, 3, 4], (num) => num === 3)).toBe(3);
   expect(find([1, 2, 3, 4], (num) => num * num > 4)).toBe(3);
+  expect(find([10, 50, -20, -10, 100], (v) => v < 0)).toBe(-20);
+  expect(find([10, 50, -20, -10, 100], (v) => v < -500)).toBe(undefined);
 });
 
 test("find returns undefined when none of the items are true according to a predicate function", () => {
   expect(find([1, 2, 3, 4], (num) => num === 9)).toBe(undefined);
   expect(find([1, 2, 3, 4], (num) => num * num > 16)).toBe(undefined);
+});
+
+test("find can handle array of strings", () => {
+  //setup
+  function isUpperCase(ch: string) {
+    return ch.toUpperCase() === ch;
+  }
+
+  // act and assert
+  expect(find("abcDeFgHI".split(""), isUpperCase)).toBe("D");
 });
 
 test("find can handle custom typed arrays", () => {
