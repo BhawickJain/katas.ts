55d4f63c798fd5764f5768b1fe85fa7c35c12989
Bhawick Jain <bhawick@gmail.com>
2022-09-10 07:41:41 +0100

    format and lint

--- src/reverse-string-recursively/reverse-string-recursively-b.ts
+++ src/reverse-string-recursively/reverse-string-recursively-b.ts
@@ -1,12 +1,17 @@
 const reverseString = (inputString: string) => {
-    return reverseStringRecursively(inputString, 0)
-}
-const reverseStringRecursively = (inputString: string, index: number):string => {
-    if (inputString.length === index) {
-        return ""
-    } else {
-        return `${reverseStringRecursively(inputString, index + 1)}${inputString[index]}`
-    }
-}
+  return reverseStringRecursively(inputString, 0);
+};
+const reverseStringRecursively = (
+  inputString: string,
+  index: number,
+): string => {
+  if (inputString.length === index) {
+    return "";
+  } else {
+    return `${reverseStringRecursively(inputString, index + 1)}${
+      inputString[index]
+    }`;
+  }
+};
 
-export default reverseString
+export default reverseString;
