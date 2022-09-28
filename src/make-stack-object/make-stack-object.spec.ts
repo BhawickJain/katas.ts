ac7562353ebaa1beb27e02ae9159eb4d8495a93c
Bhawick Jain <bhawick@gmail.com>
2022-09-28 11:36:04 +0100

    lint and format

--- src/make-stack-object/make-stack-object.spec.ts
+++ src/make-stack-object/make-stack-object.spec.ts
@@ -1,24 +1,22 @@
-import { createStack } from "./make-stack-object"
+import { createStack } from "./make-stack-object";
 
 test("SHALL have normal error free usage", () => {
-
-    const myStack = createStack()
-    expect(myStack.push(10)).toBe(1)
-    expect(myStack.push(20)).toBe(2)
-    expect(myStack.pop()).toBe(20)
-    expect(myStack.push(30)).toBe(2)
-    expect(myStack.push(40)).toBe(3)
-    expect(myStack.isEmpty()).toBe(false)
-    while (!myStack.isEmpty()) {
-        const el = myStack.pop()
-        console.log(el)
-    }
-    expect(myStack.isEmpty()).toBe(true)
-})
-
+  const myStack = createStack();
+  expect(myStack.push(10)).toBe(1);
+  expect(myStack.push(20)).toBe(2);
+  expect(myStack.pop()).toBe(20);
+  expect(myStack.push(30)).toBe(2);
+  expect(myStack.push(40)).toBe(3);
+  expect(myStack.isEmpty()).toBe(false);
+  while (!myStack.isEmpty()) {
+    const el = myStack.pop();
+    console.log(el);
+  }
+  expect(myStack.isEmpty()).toBe(true);
+});
 
 test.skip("SHALL NOT access underlying array methods", () => {
-    const myStack = createStack()
-    expect(myStack.push(10)).toBe(1)
-    expect(myStack.push(20)).toBe(2)
-})
+  const myStack = createStack();
+  expect(myStack.push(10)).toBe(1);
+  expect(myStack.push(20)).toBe(2);
+});
