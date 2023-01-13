9876c233ce7d183cf4971d7da6e4340634234fa9
BhawickJain <bhawick@outlook.com>
2023-01-13 22:21:19 +0000

    complete min-subarray-len in O(n) time

---
+++ src/min-subarray-len/min-subarray-len.spec.ts
@@ -0,0 +1,12 @@
+import { minSubArrayLen } from "./min-subarray-len"
+
+test("minSubArrayLen returns the correct min length of items needed to add up to or greater than a given sum", () => {
+    expect(minSubArrayLen([2,3,1,2,4,3], 7)).toBe(2) // -> because [4,3] is the smallest subarray
+    expect(minSubArrayLen([2,1,6,5,4], 9)).toBe(2) // -> because [5,4] is the smallest subarray
+    expect(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)).toBe(1) // -> because [62] is greater than 52
+    expect(minSubArrayLen([1,4,16,22,5,7,8,9,10],39)).toBe(3) //
+    expect(minSubArrayLen([1,4,16,22,5,7,8,9,10],55)).toBe(5) //
+    expect(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)).toBe(2) //
+    expect(minSubArrayLen([1,4,16,22,5,7,8,9,10],95)).toBe(0) //
+
+})
---
+++ src/min-subarray-len/min-subarray-len.ts
@@ -0,0 +1,28 @@
+/**
+ * find the minimum length of consecutive items which add up to or greater than a given sum
+ * time O(n), space O(1)
+ * uses mutliple pointers technique
+ * @param arr list of numbers
+ * @param sum target value the consecutive items should sum up to
+ * @returns minimum items needed
+ */
+export function minSubArrayLen(arr: number[], sum: number): number {
+    let minLen = Infinity;
+    let idxS = 0;
+    let idxE = 0;
+    let currSum = arr[0];
+    while (idxE < arr.length && idxS <= idxE) {
+        if (currSum < sum) {
+            idxE += 1;
+            currSum += arr[idxE];
+        } else {
+            const candLen = idxE - idxS + 1;
+            if (candLen < minLen) {
+                minLen = candLen;
+            }
+            currSum -= arr[idxS];
+            idxS += 1;
+        }
+    }
+    return minLen===Infinity ? 0 : minLen;
+}
