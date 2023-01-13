f042e31bc9cc7e5dc71c70b10cbbf84652d05542
BhawickJain <bhawick@outlook.com>
2023-01-13 22:23:18 +0000

    lint and format

--- src/min-subarray-len/min-subarray-len.ts
+++ src/min-subarray-len/min-subarray-len.ts
@@ -7,22 +7,22 @@
  * @returns minimum items needed
  */
 export function minSubArrayLen(arr: number[], sum: number): number {
-    let minLen = Infinity;
-    let idxS = 0;
-    let idxE = 0;
-    let currSum = arr[0];
-    while (idxE < arr.length && idxS <= idxE) {
-        if (currSum < sum) {
-            idxE += 1;
-            currSum += arr[idxE];
-        } else {
-            const candLen = idxE - idxS + 1;
-            if (candLen < minLen) {
-                minLen = candLen;
-            }
-            currSum -= arr[idxS];
-            idxS += 1;
-        }
+  let minLen = Infinity;
+  let idxS = 0;
+  let idxE = 0;
+  let currSum = arr[0];
+  while (idxE < arr.length && idxS <= idxE) {
+    if (currSum < sum) {
+      idxE += 1;
+      currSum += arr[idxE];
+    } else {
+      const candLen = idxE - idxS + 1;
+      if (candLen < minLen) {
+        minLen = candLen;
+      }
+      currSum -= arr[idxS];
+      idxS += 1;
     }
-    return minLen===Infinity ? 0 : minLen;
+  }
+  return minLen === Infinity ? 0 : minLen;
 }
