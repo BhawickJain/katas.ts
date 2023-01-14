2e2540acf3debf6db7932eac66317bec18a99823
BhawickJain <bhawick@outlook.com>
2023-01-14 21:22:40 +0000

    docs

--- src/same-frequency/same-frequency.ts
+++ src/same-frequency/same-frequency.ts
@@ -1,3 +1,9 @@
+/**
+ * returns a boolean evaluation of whether 2 numbers contains an identical frequency of digits
+ * @param n1 number 1 for comparison
+ * @param n2 number 2 for comparison
+ * @returns boolean, true when frequency of occuring digits is identical, false otherwise
+ */
 export function sameFrequency(n1: number, n2: number): boolean {
   if (`${n1}`.length !== `${n2}`.length) return false;
   const freqN1 = countDigitFrequency(n1);
@@ -9,6 +15,11 @@ export function sameFrequency(n1: number, n2: number): boolean {
   return true;
 }
 
+/**
+ * returns a dictionary of digit frequencies given a number
+ * @param num number input that will have its digits counted
+ * @returns dictionary of numbers digits in string char and their number of occurances
+ */
 function countDigitFrequency(num: number): Record<string, number> {
   const freq: Record<string, number> = {};
   const strNum = `${num}`;
