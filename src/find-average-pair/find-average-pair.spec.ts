4b6e4e26ad317e5706c2153a84d4c2e4924f8e44
BhawickJain <bhawick@outlook.com>
2023-01-15 20:17:56 +0000

    average pair search using multiple pointers technique

---
+++ src/find-average-pair/find-average-pair.spec.ts
@@ -0,0 +1,8 @@
+import { averagePair } from "./find-average-pair"
+
+test("averagePair can determine is target value exists as an average of two pairs in a sorted number list", () => {
+    expect(averagePair([1,2,3],2.5)).toBe(true)
+    expect(averagePair([1,3,3,5,6,7,10,12,19],8)).toBe(true)
+    expect(averagePair([-1,0,3,4,5,6], 4.1)).toBe(false)
+    expect(averagePair([],4)).toBe(false)
+})
---
+++ src/find-average-pair/find-average-pair.ts
@@ -0,0 +1,21 @@
+/**
+ * return boolean evalation of whether at least one pair of numbers exists in a given array that matches a target value
+ * O(n) time, O(1) space
+ * 1) set low at 0, high at last item index
+ * 2) take average of two values at indices low, high
+ * 3) if too high, make mid point index at high
+ * 4) if too low, make mid point index at low
+ * 5) iterate until average equal to target average or low === high
+ */
+export function averagePair(sortedArr: number[], target: number): boolean{
+  if (sortedArr.length < 2) return false
+  let low = 0;
+  let high = sortedArr.length - 1;
+  let avg = (sortedArr[low] + sortedArr[high])/2;
+  while (avg !== target && low !== high) {
+      if (avg > target) high = high - 1;
+      if (avg < target) low = low + 1;
+      avg = (sortedArr[low] + sortedArr[high])/2;
+  }
+  return target === avg;
+}
