1bdef5117b1e1067542c5a1f6ef1c119d4adbda8
BhawickJain <bhawick@outlook.com>
2023-01-17 19:48:59 +0000

    return countdown string sequence

--- src/count-down/count-down.ts
+++ src/count-down/count-down.ts
@@ -1,6 +1,7 @@
-const countDownFrom = (num: number): void => {
+const countDownFrom = (num: number): string[] => {
   const countDown: string[] = recurseCountDownFrom(num, []);
   // countDown.forEach((el) => console.log(el)); // uncomment for demo
+  return countDown;
 };
 
 const recurseCountDownFrom = (num: number, partial: string[]): string[] => {
